29a3c774c3671a4a8c45386600bc89147609b17e
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-11 09:06:14 +0800

    cart actions

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Text } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
 const Cart = () => {
+    const cartItems = useSelector(state => state.cartItems)
   return (
     <Text>Cart</Text>
   )
